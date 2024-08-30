f05c02ca9fb77af4f3003c08cec2ad6132c42e6a
Rahul <rahulbodh819@gmail.com>
2024-08-30 17:21:09 +0530

    create work request and do work

--- app/src/main/java/com/rahul/workmanagerapplication/MainActivity.kt
+++ app/src/main/java/com/rahul/workmanagerapplication/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.rahul.workmanagerapplication
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btn :  Button
+    private lateinit var w : WorkRequest
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,19 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btn = findViewById(R.id.button)
+
+        w = OneTimeWorkRequestBuilder<MyWorker>()
+                .build()
+
+
+        btn.setOnClickListener {
+
+            WorkManager
+                    .getInstance(this)
+                    .enqueue(w)
         }
     }
 }
